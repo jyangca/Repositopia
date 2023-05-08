e417d0a4db27039bf3f29202c7c22748b602425c
Jiho YANG <yjsm111@nuua.ai>
2023-05-09 01:10:40 +0900

    ADD: Auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,26 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/server/auth";
+import { prisma } from "@/server/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
 
 export default NextAuth(authOptions);
