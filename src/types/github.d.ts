65da639b5f37bfd7085864fa2d28bbe99203fdc7
Jiho YANG <yjsm111@nuua.ai>
2023-05-10 17:07:41 +0900

    ADD: Api response type and apiHelper

---
+++ src/types/github.d.ts
@@ -0,0 +1,66 @@
+export interface UserResponse {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  blog: string | null;
+  location: string | null;
+  email: string | null;
+  hireable: boolean | null;
+  bio: string | null;
+  twitter_username: string | null;
+  followers: number;
+}
+
+export interface RepositoryResponse {
+  owner: UserResponse;
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  html_url: string;
+  description: string | null;
+  fork: boolean;
+  url: string;
+  forks_url: string;
+  keys_url: string;
+  collaborators_url: string;
+  teams_url: string;
+  hooks_url: string;
+  issue_events_url: string;
+  events_url: string;
+  assignees_url: string;
+  branches_url: string;
+  tags_url: string;
+  blobs_url: string;
+  git_tags_url: string;
+  git_refs_url: string;
+  trees_url: string;
+  statuses_url: string;
+  languages_url: string;
+  stargazers_url: string;
+  stargazers_count: number;
+  contributors_url: string;
+  subscribers_url: string;
+  subscription_url: string;
+  commits_url: string;
+  git_commits_url: string;
+  comments_url: string;
+  issue_comment_url: string;
+  contents_url: string;
+  compare_url: string;
+  merges_url: string;
+  archive_url: string;
+  downloads_url: string;
+  issues_url: string;
+  pulls_url: string;
+  milestones_url: string;
+  notifications_url: string;
+  labels_url: string;
+  releases_url: string;
+  deployments_url: string;
+}
---
+++ src/utils/helper.ts
@@ -0,0 +1,29 @@
+import { type RepositoryResponse, type UserResponse } from "@/types/github";
+
+export const mapGithubUserToProfile = (user: UserResponse) => {
+  return {
+    github: user.login,
+    name: user.name,
+    avatar: user.avatar_url,
+    bio: user.bio,
+    location: user.location,
+    website: user.blog,
+    twitter: user.twitter_username,
+    followers: user.followers,
+    hireable: user.hireable ?? false,
+  };
+};
+
+export const mapGithubReposisotryToProject = (
+  repository: RepositoryResponse
+) => {
+  return {
+    id: repository.id,
+    name: repository.name,
+    description: repository.description,
+    url: repository.html_url,
+    star_count: repository.stargazers_count,
+    owner_id: repository.owner.id,
+    owner_name: repository.owner.login,
+  };
+};
